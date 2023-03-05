ff535f2a89cfff2a03034299e1d5cf36861f9971
ldavid94 <ldavid92394@gmail.com>
2023-03-05 15:53:43 -0600

    Cambio Tecnico reporteador

---
+++ Codigo/Componentes/Reporteador/CapaVistaReporteria/reporteria_usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CapaVistaReporteria
+{
+    public partial class reporteria_usuario : Form
+    {
+        public reporteria_usuario()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void reporteria_usuario_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabla_registro_DoubleClick(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
